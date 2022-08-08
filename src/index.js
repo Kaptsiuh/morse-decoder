28e3e68e58d5232d4a549f7e55173f6dc0298a46
Dima <dimaberry1@gmail.com>
2022-08-08 17:02:41 +0300

    feat: realized task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const entries = Object.entries(MORSE_TABLE)
+    const oneSimbol = expr.match(/.{1,10}/g);
+    let twoSimbol = '';
+    let morseArr = [];   
+    for(let i = 0; i < oneSimbol.length; i++) {
+        if (oneSimbol[i] === '**********') {
+            oneSimbol[i] = ' ';
+        }
+
+        twoSimbol = oneSimbol[i].match(/.{1,2}/g);
+        for(let j = 0; j < oneSimbol[i].length; j++) {
+            if (twoSimbol[j] === '10') {
+                twoSimbol[j] = '.';
+            } else if (twoSimbol[j] === '11') {
+                twoSimbol[j] = '-';
+            } else if (twoSimbol[j] === '00') {
+                twoSimbol[j] = '';
+            }
+        }
+        morseArr.push(twoSimbol.join(''));
+    }
+
+    for(let i = 0; i < morseArr.length; i++) {
+        for(let j = 0; j < entries.length; j++) {
+            if (morseArr[i] === entries[j][0]) {
+                morseArr[i] = entries[j][1];
+            }
+        }  
+    }
+    return String(morseArr.join(''));
 }
 
 module.exports = {
